10c558e2a721339e85498c01431ef90270f9234f
Robin Kochauf <robin@gordito.se>
2018-02-15 20:01:31 +0100

    Version two, with a real class structure this time!

--- main.js
+++ main.js
@@ -10,9 +10,9 @@
 		- The class that first ran Init() is stored in the Controller.Plugin.pluginname
 
 
-		Run(controller, session, command)
+		Run(session, command)
 		- Allocates a unique class with Run() for every session
-		- All your Inited data (Eg. MySQL, global vars, trackers...) is avaliable in Controller.Plugin.pluginname
+		- All your Inited data (Eg. MySQL, global vars, trackers...) is avaliable in this.Controller.Plugin.pluginname
 		- The parameter command is a json structure following the Klister.io pattern
 		- The parameter Session is a REST or WebSocket connection with a paretn Device. 
 			- In the Device, you can access metadata connected to the User added by Klister.io and other plugins.
@@ -22,37 +22,54 @@
 
 */
 
-var Controller = null;
-
-function Init(controller)
+class Command
 {
-	Controller = controller;
-	// Your init code, DB connections and worker jobs here...
-	// Maybe check the global Config for your required values.
+	// The command is reallocated everytime 
+	// Eg. if the module is called 'klister-testplugin' the plugin name should be set to 'testplugin'
+	constructor(controller)
+	{
+		this.Controller = controller;
+		this.PluginName = 'your-plugin-name';
+	}
+
+	// Runns when the server starts the inited version of the class
+	// is saved in this.Controller.Plugins[this.PluginName]
+	Init()
+	{
+		// Your init code, DB connections and worker jobs here...
+		// Maybe check the global Config for your required values, like this:
+
+		if (!('my_custom_config_value' in this.Controller.Config))
+		{
+			console.error('[Plugin Error] My config value is missing');
+			process.exit();
+		}
+	}
+
+	// Runs when your command is called from a client
+	Run(session, command)
+	{
+
+		// Metadata, like command name, is in the first level of the json
+		var ret 			= {};
+		ret.com 			= command.com; // Required - this will be the plugin name
+		ret.action 			= command.action; // Required - a custom action you can react to
+		ret.success 		= 1;  // Required - Return 1 of the command was successfull, 0 if not
+		ret.error 			= 0;  // Optional, but nice to have on errors - If success was 0, optionally return an error code explaining what went wrong
+		ret.errortext 		= ''; // Optional - Add a error text to clarify the error
+
+		// a successfull domain uses the data field to return the data
+		ret.data = {};
+		ret.data.test = 'Hello KLister.io 😁';
+		ret.data.another = 'Is there life on Mars?';
+
+		// Use the session to return, in [Object] format.
+		// Klister.io Session does the Json parsing for you
+		session.Send(ret);
+	}
+
+	// Add more functions, folders and stuff if you need to. 
+	// This class is only the entry point.
+
 }
-
-function Run(controller, session, command) 
-{
-	// Metadata, like command name, is in the first level of the json
-	var ret 			= {};
-	ret.com 			= 'testplugin'; // the plugin name
-	ret.action 			= 'call-mom'; // the API action
-	ret.success 		= 1;  // Return 1 of the command was successfull, 0 if not
-	ret.error 			= 0;  // If success was 0, optionally return an error code explaining what went wrong
-	ret.errortext 		= ''; // Add a error text to clarify the error
-
-	// a successfull domain uses the data field to return the data
-	ret.data = {};
-	ret.data.test = 'Hello KLister.io 😁';
-	ret.data.another = 'Is there life on Mars?';
-
-	// Use the session to return, in [Object] format.
-	// Klister.io Session does the Json parsing for you
-	session.Send(ret);
-}
-
-module.exports = 
-{
-	Init,
-	Run
-};
+module.exports = Command;
